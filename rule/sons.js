51bf7de1ecc8149a23243fb4669f18352c8f5aae
lofiey <42893670+lofiey@users.noreply.github.com>
2024-06-25 15:21:25 +0800

    Update sons.js

--- rule/sons.js
+++ rule/sons.js
@@ -107,9 +107,9 @@ let default_settings = {
     },
     Netflix: {
         type: "Google", // Google, DeepL, External, Disable
-        lang: "English",
+        lang: "JA [CC]",
         sl: "auto",
-        tl: "en",
+        tl: "zh-CN",
         line: "s", // f, s
         dkey: "null", // DeepL API key
         s_subtitles_url: "null",
