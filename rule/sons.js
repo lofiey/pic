c16b4ce49f9a137a22aa7f45a61e1116f4850bc4
lofiey <42893670+lofiey@users.noreply.github.com>
2024-06-25 15:26:39 +0800

    Update sons.js

--- rule/sons.js
+++ rule/sons.js
@@ -109,7 +109,7 @@ let default_settings = {
         type: "Google", // Google, DeepL, External, Disable
         lang: "JA [CC]",
         sl: "auto",
-        tl: "zh-CN",
+        tl: "ZH",
         line: "s", // f, s
         dkey: "null", // DeepL API key
         s_subtitles_url: "null",
