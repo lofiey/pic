ce986b9663754275a3e5538e7138ebc027f4d6e0
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-01 23:42:23 +0800

    Update ytbcc.js

--- rule/ytbcc.js
+++ rule/ytbcc.js
@@ -1,12 +1,6 @@
-let url = new URL($request.url);
-
-// 统一强制字幕为简体中文
-url.searchParams.set("lang", "zh-Hans");
-
-// 强制加入机翻语言参数
-url.searchParams.set("tlang", "zh-Hans");
-
-// 修复 YouTube APP 不稳定的 hl 参数（UI 语言），确保不冲突
-url.searchParams.set("hl", "zh-CN");
-
-$done({ url: url.toString() });
+let reqUrl = $request.url;
+reqUrl = reqUrl.replace(/([?&])lang=[^&]+/i, "$1lang=zh-Hans");
+if (!/tlang=/.test(reqUrl)) {
+    reqUrl += "&tlang=zh-Hans";
+}
+$done({ url: reqUrl });
