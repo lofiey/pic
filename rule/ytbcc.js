e9576e33ce875a1c70b2dd80144c372c1b0dde7c
lofiey <42893670+lofiey@users.noreply.github.com>
2025-12-01 23:52:32 +0800

    Update ytbcc.js

--- rule/ytbcc.js
+++ rule/ytbcc.js
@@ -1,6 +1,13 @@
 let reqUrl = $request.url;
 reqUrl = reqUrl.replace(/([?&])lang=[^&]+/, "$1lang=zh-Hans");
 if (!/tlang=/.test(reqUrl)) {
-    reqUrl += "&tlang=zh-Hans";
+  reqUrl += "&tlang=zh-Hans";
 }
-$done({url: reqUrl, headers: $response.headers});
+reqUrl = reqUrl.replace(/([?&])format=[^&]+/, "$1format=json3");
+if (!/format=/.test(reqUrl)) {
+  reqUrl += "&format=json3";
+}
+if (!/fmt=/.test(reqUrl)) {
+  reqUrl += "&fmt=json3";
+}
+$done({ url: reqUrl, headers: $response.headers });
